fe23e57d0ba192c8e97c25d7fc2cbbe16f039e7f
Fidel Montesino <fidel.montesino@gmail.com>
2020-04-16 14:05:22 +0100

    ThankYou screen after the form

--- app/src/main/java/com/kccreate/intelligentlockdown/healthmonitor/HealthMonitorActivity.kt
+++ app/src/main/java/com/kccreate/intelligentlockdown/healthmonitor/HealthMonitorActivity.kt
@@ -3,15 +3,12 @@ package com.kccreate.intelligentlockdown.healthmonitor
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.viewModels
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.kccreate.intelligentlockdown.R
 import com.kccreate.intelligentlockdown.extensions.observe
 import com.kccreate.intelligentlockdown.healthmonitor.SymptomsResult.*
-import com.kccreate.intelligentlockdown.healthmonitor.model.Question
 
 class HealthMonitorActivity : AppCompatActivity() {
 
@@ -31,21 +28,25 @@ class HealthMonitorActivity : AppCompatActivity() {
         replaceFragment(HowDoYouFeelFragment.newInstance())
     }
 
-    private fun onSymptomsResult(symptomsResult: SymptomsResult) {
-        when(symptomsResult){
-            LockDown -> replaceFragment(OkGreatFragment.newInstance()) //FIXME Maybe?
-            Severity1 -> replaceFragment(TipsSeverity1Fragment.newInstance())
-            Severity2 -> replaceFragment(TipsSeverity2Fragment.newInstance())
-            ContactForm -> replaceFragment(ContactFormFragment.newInstance())
-        }
-    }
-
     fun onFeelingGoodClicked() {
         replaceFragment(OkGreatFragment.newInstance())
     }
 
     fun onNotFeelingGoodClicked() {
         replaceFragment(SymptomFormFragment.newInstance())
+    }
+
+    fun onFormSubmitted() {
+        replaceFragment(ThankYouFragment.newInstance())
+    }
+
+    private fun onSymptomsResult(symptomsResult: SymptomsResult) {
+        when (symptomsResult) {
+            LockDown -> replaceFragment(OkGreatFragment.newInstance())
+            Severity1 -> replaceFragment(TipsSeverity1Fragment.newInstance())
+            Severity2 -> replaceFragment(TipsSeverity2Fragment.newInstance())
+            ContactForm -> replaceFragment(ContactFormFragment.newInstance())
+        }
     }
 
     private fun replaceFragment(fragment: Fragment) {
