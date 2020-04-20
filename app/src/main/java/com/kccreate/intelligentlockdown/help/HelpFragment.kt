a886626e572b5bcafee4349500154b9cc9345dde
Georgi Eftimov <geftimov@users.noreply.github.com>
2020-04-20 15:31:43 +0100

    Help and all translations (#13)

---
+++ app/src/main/java/com/kccreate/intelligentlockdown/help/HelpFragment.kt
@@ -0,0 +1,12 @@
+package com.kccreate.intelligentlockdown.help
+
+import androidx.fragment.app.Fragment
+import com.kccreate.intelligentlockdown.R
+
+class HelpFragment : Fragment(R.layout.fragment_help) {
+
+    companion object {
+        fun newInstance() = HelpFragment()
+    }
+
+}
